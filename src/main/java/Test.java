2012e41c868ba534e2023fd7ad12d1e590ad583b
Roman Sherstov <rshersto@montgomerycollege.edu>
2019-11-10 11:14:20 -0500

    Adding exception hadnling.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,10 +7,14 @@ import java.io.File;
 
 public class Test {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello world!");
-        String file = FileUtils.readFileToString(new File("/tmp/sample"), Charsets.toCharset("UTF-8"));
-        JsonValue value = Json.parse(file);
-        System.out.println(value.toString());
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hello world!");
+            String file = FileUtils.readFileToString(new File("/tmp/sample"), Charsets.toCharset("UTF-8"));
+            JsonValue value = Json.parse(file);
+            System.out.println(value.toString());
+        } catch (Exception e) {
+            System.out.println(String.format("Oh no! Something went wrong: %s\n", e.getMessage()));
+        }
     }
 }
